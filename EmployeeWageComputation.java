2127d6607873e8ccf8d24b64b14ac3995dd3ecc2
bhimineni-kalyani <bhiminenikalyani@gmail.com>
2021-02-20 21:59:18 +1400

    Refactor - program2 functionality added

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,5 +1,17 @@
-class EmployeeWageComputation {
+public class EmployeeWageComputation {
     public static void main(String[] args) {
-       System.out.println("Welcome to Employee Wage Computation");
+       int ispresent = 1;
+       int Wageperhour  = 20;
+       int Fulldayhour = 8;
+       int Dailypayment = 0;
+       int empCheck = (int) (Math.random() * 10) % 2 ;
+       if (ispresent == empCheck){
+            System.out.println("Employee is  Present");
+       }
+       else{
+            System.out.println("Employee is Abscent");
+           }
+       Dailypayment=Wageperhour*Fulldayhour;
+       System.out.print("\nDailypayment:"+Dailypayment);
     }
 }
