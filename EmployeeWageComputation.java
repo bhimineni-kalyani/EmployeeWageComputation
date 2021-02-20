fd0572f8d1839a63e3598fccbf1f5713dd841ee5
bhimineni-kalyani <bhiminenikalyani@gmail.com>
2021-02-20 23:03:47 +1400

    Refactor - program4 functionality added

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,17 +1,29 @@
 public class EmployeeWageComputation {
     public static void main(String[] args) {
-       int ispresent = 1;
+       int Fulltime = 1;
+       int Parttime = 2;
        int Wageperhour  = 20;
-       int Fulldayhour = 8;
-       int Dailypayment = 0;
-       int empCheck = (int) (Math.random() * 10) % 2 ;
-       if (ispresent == empCheck){
+       int Salary = 0;
+       int Employeehour = 0;
+       int empCheck = (int) (Math.random() * 10) % 3 ;
+       
+       switch(empCheck){
+          
+            case 1:
             System.out.println("Employee is  Present");
-       }
-       else{
-            System.out.println("Employee is Abscent");
-           }
-       Dailypayment=Wageperhour*Fulldayhour;
-       System.out.print("\nDailypayment:"+Dailypayment);
+            Employeehour = 8;
+            break;
+       
+            case 2:
+            System.out.println("Employee is Parttime");
+            Employeehour = 4;
+            break;
+        
+            default: 
+            System.out.println("Employee is  Absent");
+            Employeehour = 0;
+        }
+    Salary=Wageperhour*Employeehour;
+    System.out.print("\nSalary:"+Salary);
     }
 }
