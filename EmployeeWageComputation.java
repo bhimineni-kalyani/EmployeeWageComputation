4a8add219981fae4e6dfd40535b453f145537f08
bhimineni-kalyani <bhiminenikalyani@gmail.com>
2021-02-21 22:35:34 +1400

    Refactor - program6 functionality added

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,17 +1,37 @@
 public class EmployeeWageComputation {
     public static void main(String[] args) {
-       int ispresent = 1;
+       int isPresent =1;
+       int Fulltime = 1;
+       int Parttime = 2;
+       int Maxhoursinmonth = 100;
        int Wageperhour  = 20;
-       int Fulldayhour = 8;
-       int Dailypayment = 0;
-       int empCheck = (int) (Math.random() * 10) % 2 ;
-       if (ispresent == empCheck){
-            System.out.println("Employee is  Present");
+       int totalsalary = 0;
+       int Totalemployeehours = 1;
+       int Employeehours = 10;
+       int Numberworkingdays = 20;
+       int totalworkingdays =  20;
+       while ( Totalemployeehours < Maxhoursinmonth &&
+              totalworkingdays < Numberworkingdays){
+           totalworkingdays++ ;
+           int empCheck = (int) (Math.random() * 10) % 3 ;
+               switch (empCheck) {  
+
+                     case 01: 
+                     System.out.println("\nEmployee is  Present");
+                     Employeehours = 8;
+                     break;
+       
+                     case 02:
+                     System.out.println("\nEmployee is Parttime");
+                     Employeehours = 4;
+                     break;
+        
+                     default:
+                     System.out.println("\nEmployee is  Absent");
+                     Employeehours = 0;
+              }
+       Totalemployeehours=Totalemployeehours*Employeehours;
+       System.out.println("\nTotalEmployeehours:"+ Wageperhour);
        }
-       else{
-            System.out.println("Employee is Abscent");
-           }
-       Dailypayment=Wageperhour*Fulldayhour;
-       System.out.print("\nDailypayment:"+Dailypayment);
     }
-}
+}    
